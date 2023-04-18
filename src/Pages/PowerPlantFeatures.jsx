7cb510b2809991278ac955847efb422e35ed375b
unknown <helmetnipapa@gmail.com>
2023-04-18 19:30:57 +0800

    Added PowerPlantFeatures and Protocol views [jsx]

---
+++ src/Pages/PowerPlantFeatures.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PowerPlantFeatures() {
+    return (
+        <div>
+            <h1>Power Plant Features</h1>
+        </div>
+    )
+}
---
+++ src/Pages/Protocols.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Protocols() {
+    return (
+        <div>
+            <h1>Protocols</h1>
+        </div>
+    )
+}
