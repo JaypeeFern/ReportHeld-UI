cef7e6f6e14268aa2116f4a29ec9305dc076aa67
unknown <helmetnipapa@gmail.com>
2023-04-24 16:22:56 +0800

    [PageNotFound] added text and icon

--- src/Util/PageNotFound.jsx
+++ src/Util/PageNotFound.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Icon from '@mdi/react';
+import { mdiExclamationThick } from '@mdi/js';
 
 export default function PageNotFound() {
     return (
-        <>
-        <h1>Page not Found</h1>
-        </>
+        <div className='flex-grow flex items-center justify-center me-7 text-7xl '>
+            <h1 className='font-bold'>PAGE NOT FOUND</h1>
+            <Icon path={mdiExclamationThick} size={4}/>
+        </div>
     )
 }
